4e2a47bd06d096100fee050327051071ec5b379d
Miles Singleton <milessingleton@hotmail.co.uk>
2020-12-18 09:41:29 +0000

    added tailwind css

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('posts', 'App\Http\Controllers\PostController@index')->name('posts.index');
-Route::get('posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
-Route::post('posts', 'App\Http\Controllers\PostController@store')->name('posts.store');
-
-///Comments route
-Route::get('comments/create','App\Http\Controllers\CommentController@create')->name('comments.create');
-Route::post('comments', 'App\Http\Controllers\CommentController@store')->name('comments.store');
-
-//Show must be the last route
-Route::get('posts/{id}', 'App\Http\Controllers\PostController@show')->name('posts.show');
-
 require __DIR__.'/auth.php';
