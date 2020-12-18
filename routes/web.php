a41a9d5e162a148caa94496040d3892c20878d24
Miles Singleton <milessingleton@hotmail.co.uk>
2020-12-18 09:44:43 +0000

    added route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+//TODO add auth to create 
+Route::get('posts', 'App\Http\Controllers\PostController@index')->name('posts.index');
+Route::get('posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
+Route::post('posts', 'App\Http\Controllers\PostController@store')->name('posts.store');
+
+///Comments route
+Route::get('comments/create','App\Http\Controllers\CommentController@create')->name('comments.create');
+Route::post('comments', 'App\Http\Controllers\CommentController@store')->name('comments.store');
+
+//Show must be the last route
+Route::get('posts/{id}', 'App\Http\Controllers\PostController@show')->name('posts.show');
