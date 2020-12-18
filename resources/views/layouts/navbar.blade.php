94eac42773907851aaa8d658a68983c2becc2c9e
Miles Singleton <milessingleton@hotmail.co.uk>
2020-12-18 13:44:47 +0000

    added navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="text-gray-500 bg-gray-900 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
+                viewBox="0 0 24 24">
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-xl">tailblocks</span>
+        </a>
+        <nav
+            class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-white" href='/posts/'>Posts</a>
+            <a class="mr-5 hover:text-white"href='/dashboard/'>Account</a>
+        </nav>
+    </div>
+</header>
--- resources/views/layouts/postsLayout.blade.php
+++ resources/views/layouts/postsLayout.blade.php
@@ -1,32 +1,52 @@
 <!doctype html>
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-<html lang ="en"> 
-   <head>
-    <section class="text-gray-500 body-font bg-gray-900">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-wrap w-full mb-20">
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+@include('layouts.navbar')
+<section class="text-gray-500 body-font bg-gray-900">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap w-full mb-20">
             <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Miles' Messeageboard</h1>
-              <div class="h-1 w-20 bg-purple-500 rounded"></div>
+                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Miles' Messeageboard</h1>
+                <div class="h-1 w-20 bg-purple-500 rounded"></div>
             </div>
-            <p class="lg:w-1/2 w-full leading-relaxed text-base"><a href="{{ route('posts.create')}}">Create new post</a></p>
-          </div>
-        </head> 
-        <body>
+            <p class="lg:w-1/2 w-full leading-relaxed text-base"><a href="{{ route('posts.create') }}">Create new
+                    post</a></p>
+        </div>
+
         @if ($errors->any())
             <div>
                 Errors:
                 <ul>
                     @foreach ($errors->all() as $error)
-                        <li>{{$error}}</li>  
+                        <li>{{ $error }}</li>
                     @endforeach
                 </ul>
         @endif
+       
         <div>
             @yield('content')
         </div>
-    
-    </body>
+
+
+
 </section>
+
 </html>
