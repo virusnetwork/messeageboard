87c106cf5f13634fac0c02843e14162b80ae613b
Miles Singleton <milessingleton@hotmail.co.uk>
2020-12-23 09:26:21 +0000

    changed index image (user image broken)

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,14 +5,21 @@
 @section('content')
     @if (session('message'))
         <p><b>{{ session('message') }}</b></p>
-    @endif 
+    @endif
     <div class="flex flex-wrap -m-4">
         @foreach ($posts as $post)
-            <div onclick="location.href='/posts/{{$post->id}}';" style="cursor: pointer;" class="xl:w-1/4 md:w-1/2 p-4">
+            <div onclick="location.href='/posts/{{ $post->id }}';" style="cursor: pointer;" class="xl:w-1/4 md:w-1/2 p-4">
                 <div class=" bg-gray-800 p-6 rounded-lg">
-                    <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401"
-                        alt="content">
-                    <h3 class="tracking-widest text-purple-500 text-xs font-medium title-font">by {{  App\Models\User::find($post->user_id)->username  }}</h3>
+                    @if (isset($post->image_name))
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                        src={{ Storage::disk('public')->url($post->image_name) }}alt="content">
+                    @else
+                        <img class="h-40 rounded w-full object-cover object-center mb-6"
+                        src={{ Storage::disk('public')->url('noImage.jpg')}}  alt="content">
+                    @endif
+
+                    <h3 class="tracking-widest text-purple-500 text-xs font-medium title-font">by
+                        {{ App\Models\User::find($post->user_id)->username }}</h3>
                     <h2 class="text-lg text-white font-medium title-font mb-4"> {{ $post->title }}</h2>
                     <p class="leading-relaxed text-base">
                         {{ \Illuminate\Support\Str::limit($post->content, 100, $end = '...') }}
